0a096f437d92e3a69c0843bdad0fe77d1d6bf892
Yalluri Javya <yallurijavya@gmail.com>
2025-10-19 23:36:33 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,14 @@
+def chatbot():
+    print("Chatbot:Hello!Type 'bye' to exit.")
+    while True:
+        user_input=input("You:").lower()
+        if user_input == "hello":
+            print("Chatbot:Hi!")
+        elif user_input == "how are you":
+            print("Chatbot:I'm good!How about you?")
+        elif user_input == "bye":
+            print("Chatbot:Bye!have a good day!")
+            break
+        else:
+            print("Chatbot:Sorry,I don't understand.")
+chatbot()
